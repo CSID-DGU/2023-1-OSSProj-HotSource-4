6ddee9cee39f02b37950bb125829ffcfb7226814
a0100019 <dldbqls0019@naver.com>
2023-06-04 00:28:15 +0900

    23-06-04 메시지 query isCurrentUser 추가

--- server/controller/query/query.message.js
+++ server/controller/query/query.message.js
@@ -4,24 +4,28 @@ import { Group } from "../../models/group.model.js";
 import { requireAuth } from "../../user.permission.js";
 
 export const queryMessages = async (_, { groupId }, { user }) => {
-    requireAuth(user);
-    const group = await Group.findById(groupId);
-    if (!group.members.includes(user._id)) {
-      throw new AuthenticationError("Unauthorized");
-    }
-    
-    const messages = await Message.find({ group: groupId })
-    .populate({
-      path: 'user',
-      select: 'username',
-    })
-    .sort({ createdAt: 1 });
+  requireAuth(user);
+  const group = await Group.findById(groupId);
+  if (!group.members.includes(user._id)) {
+    throw new AuthenticationError("Unauthorized");
+  }
   
-    // 각 메시지에 isCurrentUser필드를 추가하여 본인의 메시지인지 아닌지 알 수 있음
-    messages.forEach(message => {
-      message.isCurrentUser = String(message.user._id) === String(user._id);
-    });
-  
-    return messages;
-  };
-  
+  const messages = await Message.find({ group: groupId })
+  .populate({
+    path: 'user',
+    select: 'username',
+  })
+  .populate({
+    path: 'group',
+    select: 'name',
+  })
+  .sort({ createdAt: 1 });
+
+  const result = messages.map(message => ({
+    ...message._doc,
+    isCurrentUser: String(message.user._id) === String(user._id),
+    groupName: message.group.name,
+  }));
+
+  return result;
+};
